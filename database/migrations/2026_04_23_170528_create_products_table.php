9a44aba631b7ae5db5ad5c2ad338cff28546af2a
Limon Saikia <saikialimon310@gmail.com>
2026-04-28 21:23:28 +0530

    Initial commit - Laravel eCommerce project

---
+++ database/migrations/2026_04_23_170528_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('category_id');
+            $table->string('product_name',255);
+            $table->string('image',255);
+            $table->double('price');
+            $table->double('discount');
+            $table->integer('avail_count');
+            $table->integer('booked_count');
+
+            $table->timestamps();
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
