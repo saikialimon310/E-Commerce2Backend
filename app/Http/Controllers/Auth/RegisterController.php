d926d62839bae04e4ea9b0b534464574a242094a
Limon Saikia <saikialimon310@gmail.com>
2026-06-22 21:42:37 +0530

    latest

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,7 +65,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'role' => 'seller',
+            'user_type' => 'Seller',
             'status' => 'active',
         ]);
     }
