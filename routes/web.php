02680405e004291e2fd49a9bf06dcd26cda053cc
Barjo3biswa <biswajit2013laskar@gmail.com>
2026-06-24 19:13:34 +0530

    latest

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\MyCartController;
 use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    return redirect()->route('home');
+    return redirect()->route('login');
 });
 
 Auth::routes();
